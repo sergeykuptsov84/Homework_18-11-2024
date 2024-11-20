337ce7cd1ceff36968dca0e91cf2bf450e4b6467
Sergey Kuptsov <skuptsov1984@gmail.com>
2024-11-20 17:16:27 +0300

    20/11 file variable added

---
+++ variable.py
@@ -0,0 +1,5 @@
+home_work_numbers = 12
+hours_spent = 1.5
+name_of_lesson = "Python"
+hours_for_lesson = (1.5/12)
+print ('Курс Python ,'' всего задач:',home_work_numbers, ' , затрачено часов:',hours_spent,', среднее время выполнения:',hours_for_lesson,'часа')
