11c4a8b97a762d715c77dedcda9415b6a3cc19c1
sergeykuptsov84 <skuptsov1984@gmail.com>
2024-11-23 08:45:25 +0300

    Delete module_1_3.py

--- module_1_3.py
+++
@@ -1,8 +0,0 @@
-name = 'Sergey'  # variable str
-print('Name:' ,name)
-Age = 64             # variable int
-print('Age:', Age)
-Age = (Age+1)        # variable int
-print('New Age: ', Age)
-is_student = True  # variable bool
-print('Is Student:', is_student)
